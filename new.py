c1a6998923be90dd8718ee8ab7479e12c625463c
iconic girl <hawaawangari@gmail.com>
2024-12-21 15:03:41 +0300

    commit new changes

---
+++ new.py
@@ -0,0 +1,10 @@
+my_list =[]
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.sort()
+index_of_30 =my_list.index(30)
+print(f"the index of the 30 in my_list is : {index_of_30}")
